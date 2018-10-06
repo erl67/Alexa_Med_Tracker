e7740a96745f7e798eaa75eff2699fa383c1c95b
erl67 <erl67@pitt.edu>
2018-10-06 05:28:47 -0400

    alpha

--- README.md
+++ README.md
@@ -21,8 +21,13 @@
 
 ### Useful Links
 https://github.com/johnwheeler/flask-ask
+
 https://github.com/johnwheeler/flask-ask/pull/231#issuecomment-407593414
+
 https://developer.amazon.com/blogs/post/Tx14R0IYYGH3SKT/Flask-Ask-A-New-Python-Framework-for-Rapid-Alexa-Skills-Kit-Development
+
 https://github.com/chrisvoncsefalvay/cookiecutter-flask-ask
+
 https://dashboard.ngrok.com/get-started
+
 https://stackoverflow.com/questions/29846087/microsoft-visual-c-14-0-is-required-unable-to-find-vcvarsall-bat
--- meds.py
+++ meds.py
@@ -4,27 +4,25 @@ import logging, os, re
 from datetime import datetime
 from flask import Flask, json, render_template
 from flask_ask import Ask, request, session, question, statement
-from sys import stderr
-
-# __author__ = '{{ cookiecutter.author_name }}'
-# __email__ = '{{ cookiecutter.author_email_address }}'
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+med_count = 5 
+day_streak = 3
+reward_delta = 8
+points = 100
+
 # Session starter
-#
-# This intent is fired automatically at the point of launch (= when the session starts).
-# Use it to register a state machine for things you want to keep track of, such as what the last intent was, so as to be
-# able to give contextual help.
 
 @ask.on_session_started
 def start_session():
     """
     Fired at the start of the session, this is a great place to initialise state variables and the like.
     """
-    logging.debug("Session started at {}".format(datetime.now().isoformat()))
+    print("👁️‍🗨️")
+    logging.debug("👁️‍🗨️Session started at {}".format(datetime.now().isoformat()))
 
 # Launch intent
 #
@@ -50,110 +48,87 @@ def handle_launch():
     welcome_re_text = render_template('welcome_re')
     welcome_card_text = render_template('welcome_card')
 
-    return question(welcome_text).reprompt(welcome_re_text).standard_card(title="Med Schedule",
+    return question(welcome_text).reprompt(welcome_re_text).standard_card(title="Pills Here",
                                                                           text=welcome_card_text)
 
 
 # Built-in intents
-#
-# These intents are built-in intents. Conveniently, built-in intents do not need you to define utterances, so you can
-# use them straight out of the box. Depending on whether you wish to implement these in your application, you may keep
-# or delete them/comment them out.
-#
 # More about built-in intents: http://d.pr/KKyx
-
 @ask.intent('AMAZON.StopIntent')
 def handle_stop():
-    """
-    (STATEMENT) Handles the 'stop' built-in intention.
-    """
     farewell_text = render_template('stop_bye')
     return statement(farewell_text)
-
-
 @ask.intent('AMAZON.CancelIntent')
 def handle_cancel():
-    """
-    (STATEMENT) Handles the 'cancel' built-in intention.
-    """
     farewell_text = render_template('cancel_bye')
     return statement(farewell_text)
-
-
 @ask.intent('AMAZON.HelpIntent')
 def handle_help():
-    """
-    (QUESTION) Handles the 'help' built-in intention.
-    You can provide context-specific help here by rendering templates conditional on the help referrer.
-    """
-
     help_text = render_template('help_text')
     return question(help_text)
-
-
 @ask.intent('AMAZON.NoIntent')
 def handle_no():
-    """
-    (?) Handles the 'no' built-in intention.
-    """
     pass
-
 @ask.intent('AMAZON.YesIntent')
 def handle_yes():
-    """
-    (?) Handles the 'yes'  built-in intention.
-    """
     pass
-
-
 @ask.intent('AMAZON.PreviousIntent')
 def handle_back():
-    """
-    (?) Handles the 'go back!'  built-in intention.
-    """
     pass
-
 @ask.intent('AMAZON.StartOverIntent')
 def start_over():
-    """
-    (QUESTION) Handles the 'start over!'  built-in intention.
-    """
     pass
 
+@ask.intent('PillsIntent')
+def pills():
+    text = render_template('pills')
+    return statement(text).standard_card(title="Pills Here", text=text)
 
-@ask.intent('HelloWorldIntent')
-def hello_world():
-    speech_text = 'Hello world'
-    return statement(speech_text).simple_card('HelloWorld', speech_text)
+@ask.intent('GiveMedsIntent')
+def give_meds():
+    text = render_template('give_meds', med_count=med_count)
+    return statement(text).standard_card(title="Your Medications", text=text)
+
+@ask.intent('TakeMedsIntent')
+def take_meds():
+    text = render_template('take_meds') #add reprompt
+    return statement(text).standard_card(title="Did you take your meds?", text=text)
+
+@ask.intent('UpdateIntent')
+def update_meds():
+    text = render_template('update_success') #add reprompt
+    return statement(text).standard_card(title="Medications Updated", text=text)
+
+@ask.intent('ProgressIntent')
+def rewards():
+    text = render_template('rewards_prog', reward_delta=reward_delta) #add reprompt
+    return statement(text).standard_card(title="Rewards Status", text=text)
+
+@ask.intent('ReportIntent')
+def progress_report():
+    text = render_template('report', day_streak=day_streak) #add reprompt
+    return statement(text).standard_card(title="Status Reports", text=text)
+
+@ask.intent('PointsIntent')
+def points_report():
+    text = render_template('points', total=points) #add reprompt
+    return statement(text).standard_card(title="Points", text=text)
 
 
 @ask.session_ended
 def session_ended():
-    """
-    Returns an empty for `session_ended`.
-    .. warning::
-    The status of this is somewhat controversial. The `official documentation`_ states that you cannot return a response
-    to ``SessionEndedRequest``. However, if it only returns a ``200/OK``, the quit utterance (which is a default test
-    utterance!) will return an error and the skill will not validate.
-    """
     return statement("")
 
 @app.before_request
 def before_request():
     session['requests'] = int(session.get('requests') or 0) + 1
-    eprint("\t🔈 \t\t")
+    logging.debug("\t🔈\t\t".format(datetime.now().isoformat()))
         
 @app.before_first_request
 def before_first_request():
     session['starts'] = int(session.get('starts') or 0) + 1
-    eprint("\t🥇 \t\t")
+    logging.debug("\t🥇 \t\t".format(datetime.now().isoformat()))
     
-def eprint(*args, **kwargs):
-    print(*args, file=stderr, **kwargs)
-    
-TAG_RE = re.compile(r'<[^>]+>')
-def remove_tags(text):
-    return TAG_RE.sub('', text)
 
 if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:
